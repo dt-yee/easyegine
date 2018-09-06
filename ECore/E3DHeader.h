658aaf4f23abe341bd1acbf5a3a1ebb62e77c04f
ydt <yidaitian@foxmail.com>
2018-09-06 10:59:59 -0400

    init version

---
+++ ECore/E3DHeader.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace E3D
+{
+	struct EFrustum;
+
+	struct EVertex4D;		//vertex
+	struct EObject4D;		//object
+	struct EPlane3D;		//
+	struct EPoly4D;			//polygon based on vertex
+	struct EPolyF4D;		//poly
+	struct EPlane3D;		//3d plane
+	struct ERenderList4D;	//render list
+
+	struct EColor;
+	struct EVector2D;
+	struct EVector3D;
+	struct EVector4D;
+	struct EMatrix44;		//4*4 matrix
+
+}
