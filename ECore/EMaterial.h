6d08f66fbfe5d7fb9ac32f42c75f9f953b9ab3e9
ydt <yidaitian@foxmail.com>
2019-03-25 12:02:34 -0400

    add material head file

---
+++ ECore/EMaterial.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "ECommon.h"
+#include <map>
+#include "EGraphics.h"
+
+namespace E3D
+{
+    struct EMaterial
+    {
+        EString     name;
+        EColor      ambient; //environment reflection
+        EColor      diffuse; //diffuse reflection
+        EColor      specular; // specular reflection
+
+        EBitmap *bitmap;
+
+        EMaterial();
+        EMaterial();
+        
+        inline bool useTexture() { return bitmap && bitmap->isValid(); }
+        EColor getPixel(EFloat u, EFloat v);
+    };
+
+    typedef std::map<EString, EMaterial*>::iterator MaterialIter;
+    extern std::map<EString, EMaterial*> *GMaterials;
+
+    extern bool SetMaterial(const EString &name, EMaterial *material);
+    extern bool DestoryMaterial(const EString &name);
+    extern void DestoryAllMaterials();
+
+    //parse the material script, input the basic material and texture
+    //script has the same name as model
+    extern void ParseMaterialScript(const EString &scriptName);
+}
